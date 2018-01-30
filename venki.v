56c522012a2ae2a032b243b6434c82f742ec57f6
venkatroshan <35727735+venkatroshan@users.noreply.github.com>
2018-01-30 16:24:44 +0530

    Create venki.v

---
+++ venki.v
@@ -0,0 +1,27 @@
+
+#include<stdio.h>
+
+int main()
+
+{
+
+  int a,b,c, sum;
+
+  float d;
+
+  printf("a=10;b=5;");
+
+  scanf("%d%d%d",&a,&b,&c);
+
+  sum=a+b+c;
+
+  d=(a+b+c)/3;
+
+  printf("\nSum is %d", sum);
+
+  printf("\nAverage is  %f",d);
+
+  return 0;
+
+}
+
